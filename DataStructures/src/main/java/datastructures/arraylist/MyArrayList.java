9f05cd9afa6405bb42bdaac27ba6c5e5a3ab8000
Dmitrii Korneev <kdmitry.de@gmail.com>
2026-05-11 15:13:44 +0200

    Added ArrayList realization

--- DataStructures/src/main/java/datastructures/arraylist/MyArrayList.java
+++ DataStructures/src/main/java/datastructures/arraylist/MyArrayList.java
@@ -16,41 +16,115 @@ public class MyArrayList {
     // Custom constructor
     public MyArrayList(int capacity) {
         if (capacity <= 0) {
-            System.out.println("Initial capacity must be positive. Input: " + capacity);
+            throw new IllegalArgumentException("Capacity must be positive. Input: " + capacity);
         } else {
             list = new Integer[capacity];
         }
     }
 
-    // ######### Methods #########
+    // ######### Public methods #########
 
     // Add new element to the end
     public void add(Integer element) {
-
+        if(element == null) {
+            throw new IllegalArgumentException("Element must not be null");
+        }
+        if(size == list.length) {
+            increaseList();
+        }
+        list[size] = element;
+        size++;
     }
 
     // Add new element at the specific position
-    public void addAt(int index, Integer element) {
-
+    public void add(int index, Integer element) {
+        if(index < 0) {
+            throw new IndexOutOfBoundsException("Index must be non-negative");
+        }
+        if(index > size) {
+            throw new IndexOutOfBoundsException("Size of list is: " + size);
+        }
+        if(element == null) {
+            throw new IllegalArgumentException("Element must not be null");
+        }
+        if(size == list.length) {
+            increaseList();
+        }
+        for(int i = size - 1; i >= index; i--) {
+            list[i + 1] = list[i];
+        }
+        list[index] = element;
+        size++;
     }
 
     // Remove specific element
     public void remove(Integer element) {
-
+        int firstIndex = indexOf(element);
+        if(firstIndex != -1) {
+            deleteElement(firstIndex);
+        }
     }
 
     // Remove element from specific position
     public void removeAt(int index) {
-
+        if(index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Illegal index");
+        }
+        deleteElement(index);
     }
 
-    // Clean list
-    public void clean() {
-
+    // Size of the list
+    public int size() {
+        return size;
     }
 
-    // Search element in the list
-    public void search(Integer element) {
+    // Clear list
+    public void clear() {
+        list = new Integer[INITIAL_CAPACITY];
+        size = 0;
+    }
 
+    // Check whether the list is empty
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // Search an element in the list
+    public int indexOf(Integer element) {
+        if(element == null) {
+            return -1;
+        }
+        for(int i = 0; i < size; i++) {
+            if(element.equals(list[i])) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // Get an element from the list
+    public Integer get(int index) {
+        if(index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Illegal index");
+        }
+        return list[index];
+    }
+
+    // ######### Auxiliary methods #########
+
+    // Increase the array size
+    private void increaseList() {
+        Integer[] newList = new Integer[size + size/2 + 1];
+        System.arraycopy(list, 0, newList, 0, size);
+        list = newList;
+    }
+
+    // Delete an element at specific index
+    private void deleteElement(int index) {
+        for(int i = index; i < size - 1; i++) {
+            list[i] = list[i + 1];
+        }
+        list[size - 1] = null;
+        size--;
     }
 }
